eb39c6e09ab9796885e8942c316f65e212ab8828
valen <valen@DESKTOP-O1NL7RD>
2025-06-10 10:03:32 +0200

    models generate with EF and OrderDbContext

---
+++ MSPR-bloc-4-orders/Models/Commande.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSPR_bloc_4_orders.Models;
+
+public partial class Commande
+{
+    public int IdCommande { get; set; }
+
+    public DateTime Createdate { get; set; }
+
+    public int? IdClient { get; set; }
+}
---
+++ MSPR-bloc-4-orders/Models/ProduitCommande.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSPR_bloc_4_orders.Models;
+
+public partial class ProduitCommande
+{
+    public int IdProduitcommande { get; set; }
+
+    public int IdCommande { get; set; }
+
+    public int IdProduit { get; set; }
+
+    public string? Nom { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public decimal? Prix { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Color { get; set; }
+
+    public int? Quantite { get; set; }
+}
